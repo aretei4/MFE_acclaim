a1bba77ba76bb0b2fc3cfa5a8ea641afb5eebae3
subash rout <subash.rout@outlook.com>
2023-11-14 19:36:16 +0530

    intial commit

---
+++ AcclarisCore/src/main/java/com/acclaris/moblie/lib/core/FirstCoreLibTest.kt
@@ -0,0 +1,11 @@
+package com.acclaris.moblie.lib.core
+
+import android.util.Log
+
+class FirstCoreLibTest {
+    companion object{
+        fun writeLog(msg:String){
+            Log.d("test",msg)
+        }
+    }
+}
